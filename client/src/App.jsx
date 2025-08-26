7eee2e995a0a807fa7ef2a9c79d5cdbeef8b137f
Alex <161263729+M3m0ries@users.noreply.github.com>
2025-08-26 14:40:06 -0500

    Migracion a React Admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
-import Login from './page/invitado/login';
-import RegistroEmpresa from './page/invitado/RegistroEmpresa';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import './App.css';
+import AdminIndex from "./page/admin/AdminIndex";
+import AdminUsuarios from "./page/admin/AdminUsuarios";
+import AdminVacantes from "./page/admin/AdminVacantes";
 import Index from './page/invitado/Index';
-import RegistroCandidato from "./page/invitado/RegistroCandidato";
-import Registro from "./page/invitado/Registro";
 import InfoVacante from "./page/invitado/InfoVacante";
+import Login from './page/invitado/login';
+import Registro from "./page/invitado/Registro";
+import RegistroCandidato from "./page/invitado/RegistroCandidato";
+import RegistroEmpresa from './page/invitado/RegistroEmpresa';
 
 function App() {
 
@@ -13,11 +16,14 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login/>} />
         <Route path="/registro/empresa" element={<RegistroEmpresa />} />
         <Route path="/registro/candidato" element={<RegistroCandidato />} />
         <Route path="/registro" element={<Registro />} />
         <Route path="/empleos/1" element={<InfoVacante />} />
+        <Route path="/admin/index" element={<AdminIndex/>} /> 
+        <Route path="/admin/vacantes" element={<AdminVacantes/>} />
+         <Route path="/admin/usuarios" element={<AdminUsuarios/>} />
       </Routes>
     </Router>
   )
